29695a6097178b58b4651f0eb0ec4a8e558ab820
Yuvadi29 <adityatrivedi612@gmail.com>
2022-11-28 23:05:09 +0530

    Fetch all categories

--- client/src/components/Bar/Bar.jsx
+++ client/src/components/Bar/Bar.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './Bar.css';
 import barimage from './barimage.jpg';
 import { FaFacebook, FaInstagram } from 'react-icons/fa';
@@ -6,6 +8,15 @@ import { GrTwitter } from 'react-icons/gr';
 
 
 const Bar = () => {
+    const [cats, setCats] = useState([]);
+
+    useEffect(() => {
+        const getCats = async () => {
+            const res = await axios.get("http://localhost:5000/api/categories");
+            setCats(res.data);
+        }
+        getCats(); 
+    },[]);
     return (
         <div className='Bar'>
             <div className="Baritem">
@@ -18,11 +29,9 @@ const Bar = () => {
             <div className="Baritem">
                 <span className="Bartitle">Categories</span>
                 <ul className="BarList">
-                    <li className="BarListItem">Life</li>
-                    <li className="BarListItem">Animals</li>
-                    <li className="BarListItem">Music</li>
-                    <li className="BarListItem">Style</li>
-                    <li className="BarListItem">Tech</li>
+                    {cats.map(c=>(
+                        <li className="BarListItem">{c.name}</li>
+                    ))}
                 </ul>
             </div>
             <div className="Baritem">
