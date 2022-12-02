aa8234b60b192523dcd2c19802ad544b80a39fec
Yuvadi29 <adityatrivedi612@gmail.com>
2022-12-02 21:42:58 +0530

    Delete Function

--- client/src/components/SinglePost/SinglePost.jsx
+++ client/src/components/SinglePost/SinglePost.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useContext, useEffect, useState, } from 'react';
 import './SinglePost.css';
 import { BiEdit } from 'react-icons/bi';
 import { FaTrash } from 'react-icons/fa';
 import { useLocation, Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Context } from '../../context/Context';
 
 const SinglePost = () => {
     const PF = "http://localhost:5000/images/";
-
+    const { user } = useContext(Context);
     const location = useLocation();
     const path = (location.pathname.split("/")[2]);
     const [post, setPost] = useState({});
@@ -21,6 +21,19 @@ const SinglePost = () => {
         getPost();
     }, [path]);
 
+
+    //Delete Function
+    const handleDelete = async () => {
+        try {
+            await axios.delete(`http://localhost:5000/api/posts/${post._id}`, {
+                data: { username: user.username },
+            });
+            window.location.replace("/");
+        } catch (error) {
+
+        }
+    };
+
     return (
         <div className='SinglePost'>
             <div className="wrapper">
@@ -32,10 +45,13 @@ const SinglePost = () => {
 
                 < h1 className="postTitle">
                     {post.title}
+                    {post.username === user?.username} && (
+
                     <div className="edit">
                         <BiEdit className="editicon" />
-                        <FaTrash className="deleteicon" />
+                        <FaTrash className="deleteicon" onClick={handleDelete} />
                     </div>
+                    )
                 </h1>
 
                 <div className="postInfo">
